5d72735a43b8922f27173971f15e3eff203c0f71
Fernando Javier Cuadro <68226901+FernandoJavierCuadro@users.noreply.github.com>
2021-03-11 16:48:55 -0300

    added rollup

--- packages/millicast-sdk-js/rollup.config.js
+++ packages/millicast-sdk-js/rollup.config.js
@@ -1,3 +1,21 @@
-export default {
-  input: 'src/main.js',
-}
+import pkg from './package.json'
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: 'src/index.js',
+    output: {
+      name: 'millicast',
+      file: pkg.browser,
+      format: 'umd',
+    },
+  },
+
+  {
+    input: 'src/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs' },
+      { file: pkg.module, format: 'es' },
+    ],
+  },
+]
---
+++ packages/millicast-sdk-js/tests/index.js
@@ -0,0 +1,4 @@
+import MillicastWebRTC from '../src/MillicastWebRTC'
+import MillicastMedia from '../src/MillicastMedia'
+
+export { MillicastWebRTC, MillicastMedia }
