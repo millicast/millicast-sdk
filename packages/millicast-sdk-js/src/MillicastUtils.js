4c8373f594661ffd3cd5fcd0c2e88a6ae8f064de
Daiana Aysa <daiana@qualabs.com>
2021-03-22 12:52:52 -0300

    (MillicastUtils): Create request functions with axios

--- packages/millicast-sdk-js/src/MillicastUtils.js
+++ packages/millicast-sdk-js/src/MillicastUtils.js
@@ -1,27 +1,56 @@
+import axios from 'axios'
+
 export default class MillicastUtils {
-  static request(url, method, token, payload) {
-    return new Promise((resolve, reject) => {
-      let xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = (evt) => {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-          let res = JSON.parse(xhr.responseText);
-          //console.log(res);
-          switch (xhr.status) {
-            case 200:
-              resolve(res);
-              break;
-            default:
-              reject(res);
-              break;
-          }
-        }
-      };
-      xhr.open(method, url, true);
-      xhr.setRequestHeader("Content-Type", "application/json;");
-      if (!!token) {
-        xhr.setRequestHeader("Authorization", `Bearer ${token}`);
+  director (url, token, payload) {
+    return (dispatch) => {
+      const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
       }
-      !!payload ? xhr.send(JSON.stringify(payload)) : xhr.send();
-    });
+
+      axios.post(url, payload, headers).then(response => {
+        return response
+      })
+    }
+  }
+
+  turn (url, token, payload) {
+    return (dispatch) => {
+      const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      }
+
+      axios.put(url, payload, headers).then(response => {
+        return response
+      })
+    }
   }
 }
+
+  // static request(url, method, token, payload) {
+  //   return new Promise((resolve, reject) => {
+  //     let xhr = new XMLHttpRequest();
+  //     xhr.onreadystatechange = (evt) => {
+  //       if (xhr.readyState === XMLHttpRequest.DONE) {
+  //         let res = JSON.parse(xhr.responseText);
+  //         //console.log(res);
+  //         switch (xhr.status) {
+  //           case 200:
+  //             resolve(res);
+  //             break;
+  //           default:
+  //             reject(res);
+  //             break;
+  //         }
+  //       }
+  //     };
+  //     xhr.open(method, url, true);
+  //     xhr.setRequestHeader("Content-Type", "application/json;");
+  //     if (!!token) {
+  //       xhr.setRequestHeader("Authorization", `Bearer ${token}`);
+  //     }
+  //     !!payload ? xhr.send(JSON.stringify(payload)) : xhr.send();
+  //   });
+  // }
+
