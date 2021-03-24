f6fdc80df90ca80f2682eaf4e08c3d1c5072c64d
Renzo Delfino <renzo@qualabs.com>
2021-03-24 18:07:23 -0300

    Fixes in connect method

--- packages/millicast-sdk-js/src/MillicastView.js
+++ packages/millicast-sdk-js/src/MillicastView.js
@@ -7,23 +7,23 @@ import MillicastDirector from "./MillicastDirector.js";
 export default class MillicastView {
     constructor() {
         this.webRTCPeer = new MillicastWebRTC();
-        this.millicastSignaling = null
+        this.millicastSignaling = new MillicastSignaling()
     }
 
     async connect(options = {streamAccountId: null, streamName: null, disableVideo: false, disableAudio: false}){
       let setRTCRemoteSDPResponse = null
       const directorResponse = await MillicastDirector.getSubscriber(options.streamAccountId, options.streamName)
-      this.millicastSignaling = new MillicastSignaling({url: directorResponse.wsUrl})
+      this.millicastSignaling.wsUrl = `${directorResponse.wsUrl}?token=${directorResponse.jwt}`
       const rtcConfiguration = await this.webRTCPeer.getRTCConfiguration()
       const peer = await this.webRTCPeer.getRTCPeer(rtcConfiguration)
       this.webRTCPeer.RTCOfferOptions = {
         offerToReceiveVideo: !options.disableVideo,
         offerToReceiveAudio: !options.disableAudio,
       }
-      const description = await this.webRTCPeer.getRTCLocalSDP(true, null)
-      const subscribeResponse = await this.millicastSignaling.subscribe(description.sdp, options.streamName)
-      setRTCRemoteSDPResponse = (subscribeResponse.sdp) ? await this.webRTCPeer.setRTCRemoteSDP(subscribeResponse.sdp) : null
-      if(!setRTCRemoteSDPResponse){
+      const localSDP = await this.webRTCPeer.getRTCLocalSDP(true, null)
+      const SDPSubscriber = await this.millicastSignaling.subscribe(localSDP, options.streamName)
+      setRTCRemoteSDPResponse = (SDPSubscriber) ? await this.webRTCPeer.setRTCRemoteSDP(SDPSubscriber) : null
+      if(!SDPSubscriber){
         logger.error('Failed to connect to publisher: ', setRTCRemoteSDPResponse)
         throw new Error('Failed to connect to publisher: ', setRTCRemoteSDPResponse)
       }
