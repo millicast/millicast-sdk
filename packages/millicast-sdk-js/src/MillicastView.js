4fe5807f6dff0320d719bbeb104399304beb1535
Renzo Delfino <renzo@qualabs.com>
2021-03-25 10:45:07 -0300

    Added peer emit in MillicastView. Added test class and html.

--- packages/millicast-sdk-js/src/MillicastView.js
+++ packages/millicast-sdk-js/src/MillicastView.js
@@ -1,21 +1,30 @@
 import Logger from './Logger'
 const logger = Logger.get('MillicastView')
+import EventEmitter from "events";
 import MillicastSignaling from './MillicastSignaling'
 import MillicastWebRTC from "./MillicastWebRTC.js";
 import MillicastDirector from "./MillicastDirector.js";
 
-export default class MillicastView {
+export default class MillicastView extends EventEmitter {
     constructor() {
-        this.webRTCPeer = new MillicastWebRTC();
-        this.millicastSignaling = new MillicastSignaling()
+      super()
+      this.webRTCPeer = new MillicastWebRTC();
+      this.millicastSignaling = new MillicastSignaling()
     }
 
     async connect(options = {streamAccountId: null, streamName: null, disableVideo: false, disableAudio: false}){
+      logger.info(`Connecting to publisher. Stream account: ${options.streamAccountId}, stream name: ${options.streamName}`)
+      logger.debug('All viewer connect options values: ', options)
       let setRTCRemoteSDPResponse = null
       const directorResponse = await MillicastDirector.getSubscriber(options.streamAccountId, options.streamName)
       this.millicastSignaling.wsUrl = `${directorResponse.wsUrl}?token=${directorResponse.jwt}`
       const rtcConfiguration = await this.webRTCPeer.getRTCConfiguration()
       const peer = await this.webRTCPeer.getRTCPeer(rtcConfiguration)
+      peer.ontrack = (event) => {
+        logger.info("New track from peer.")
+        logger.debug("Track event value: ", event)
+        this.emit('new.track', event)
+      }
       this.webRTCPeer.RTCOfferOptions = {
         offerToReceiveVideo: !options.disableVideo,
         offerToReceiveAudio: !options.disableAudio,
@@ -24,10 +33,10 @@ export default class MillicastView {
       const SDPSubscriber = await this.millicastSignaling.subscribe(localSDP, options.streamName)
       setRTCRemoteSDPResponse = (SDPSubscriber) ? await this.webRTCPeer.setRTCRemoteSDP(SDPSubscriber) : null
       if(!SDPSubscriber){
-        logger.error('Failed to connect to publisher: ', setRTCRemoteSDPResponse)
-        throw new Error('Failed to connect to publisher: ', setRTCRemoteSDPResponse)
+        logger.error('Failed to connect to publisher: ', SDPSubscriber)
+        throw new Error('Failed to connect to publisher: ', SDPSubscriber)
       }
 
-      return setRTCRemoteSDPResponse
+      return SDPSubscriber
     }
 }
