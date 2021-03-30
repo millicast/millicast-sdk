119115f218591e399cdc2a260447e7468fb721c4
Emil Santurio <emilsas@gmail.com>
2021-03-30 12:19:38 -0300

    Refactor MillicastView

--- packages/millicast-sdk-js/src/MillicastView.js
+++ packages/millicast-sdk-js/src/MillicastView.js
@@ -11,10 +11,18 @@ export default class MillicastView extends EventEmitter {
     this.millicastSignaling = new MillicastSignaling()
   }
 
-  async connect (options = { subscriberData: null, streamName: null, disableVideo: false, disableAudio: false }) {
+  async connect (
+    options = {
+      subscriberData: null,
+      streamName: null,
+      disableVideo: false,
+      disableAudio: false
+    }
+  ) {
     logger.info(`Connecting to publisher. Stream name: ${options.streamName}`)
     logger.debug('All viewer connect options values: ', options)
     this.millicastSignaling.wsUrl = `${options.subscriberData.wsUrl}?token=${options.subscriberData.jwt}`
+
     const rtcConfiguration = await this.webRTCPeer.getRTCConfiguration()
     const peer = await this.webRTCPeer.getRTCPeer(rtcConfiguration)
     peer.ontrack = (event) => {
@@ -22,19 +30,21 @@ export default class MillicastView extends EventEmitter {
       logger.debug('Track event value: ', event)
       this.emit('new.track', event)
     }
+
     this.webRTCPeer.RTCOfferOptions = {
       offerToReceiveVideo: !options.disableVideo,
       offerToReceiveAudio: !options.disableAudio
     }
-    const localSDP = await this.webRTCPeer.getRTCLocalSDP(true, null)
-    const SDPSubscriber = await this.millicastSignaling.subscribe(localSDP, options.streamName)
-    if (SDPSubscriber) {
-      await this.webRTCPeer.setRTCRemoteSDP(SDPSubscriber)
+    const localSdp = await this.webRTCPeer.getRTCLocalSDP(true, null)
+
+    const sdpSubscriber = await this.millicastSignaling.subscribe(localSdp, options.streamName)
+    if (sdpSubscriber) {
+      await this.webRTCPeer.setRTCRemoteSDP(sdpSubscriber)
     } else {
-      logger.error('Failed to connect to publisher: ', SDPSubscriber)
-      throw new Error('Failed to connect to publisher: ', SDPSubscriber)
+      logger.error('Failed to connect to publisher: ', sdpSubscriber)
+      throw new Error('Failed to connect to publisher: ', sdpSubscriber)
     }
 
-    return SDPSubscriber
+    return sdpSubscriber
   }
 }
