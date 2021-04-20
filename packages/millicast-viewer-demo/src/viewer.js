6d28dc120fbb307d7742b6d00c02c47f8cbb309b
Emil Santurio <emilsas@gmail.com>
2021-04-20 16:23:49 -0300

    Add Chromecast sender in viewer demo

--- packages/millicast-viewer-demo/src/viewer.js
+++ packages/millicast-viewer-demo/src/viewer.js
@@ -45,123 +45,130 @@ const disableFull =
   disableControls;
 
 //console.log(disableVolume, disablePlay, disableFull);
-let millicastStream;
-let peer;
 let playing = false;
+let fullBtn = document.querySelector("#fullBtn");
+let video = document.querySelector("video");
+
+// MillicastView object
+let millicastView = null
+
+const newViewer = () => {
+  const millicastView = new MillicastView(streamId);
+  millicastView.on("broadcastEvent", (event) => {
+    if (!autoReconnect) return;
+  
+    let layers = event.data["layers"] !== null ? event.data["layers"] : {};
+    if (event.name === "layers" && Object.keys(layers).length <= 0) {
+      //call play logic or being reconnect interval
+      close().then(() => {
+        subscribe();
+      });
+      console.error("Feed no longer found.");
+    }
+  });
+  
+  millicastView.on("newTrack", (event) => {
+    if (!playing) addStream(event.streams[0]);
+  });
+
+  return millicastView
+}
+
+
+const togglePlay = () => {
+  if (video.paused) {
+    video.play()
+  } else {
+    video.pause();
+  }
+};
+
+const toggleFullscreen = () => {
+  let fullIcon = fullBtn.children[0];
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+    fullIcon.classList.remove("fa-compress");
+    fullIcon.classList.add("fa-expand");
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+
+      fullIcon.classList.remove("fa-expand");
+      fullIcon.classList.add("fa-compress");
+    }
+  }
+};
+
+const addStream = (stream) => {
+  //Create new video element
+  playing = true;
+  const audio = document.querySelector("audio");
+
+  if (disableVideo) {
+    if (audio) audio.srcObject = stream;
+    if (video) video.parentNode.removeChild(video);
+    togglePlay();
+  } else {
+    //Set same id
+    video.id = stream.id;
+    //Set src stream
+    //console.log('addStream');
+    if (!muted) {
+      video.removeAttribute("muted");
+    }
+    if (!autoplay) {
+      video.autoplay = false;
+      playing = false;
+      video.removeAttribute("autoplay");
+    }
+
+    video.srcObject = stream;
+    if (audio) audio.parentNode.removeChild(audio);
+  }
+};
+
+const close = () => {
+  video.srcObject = null;
+  playing = false;
+  millicastView?.millicastSignaling?.close();
+  millicastView = null
+  return Promise.resolve({});
+};
+
+const subscribe = async () => {
+  if (millicastView?.isActive()) {
+    return
+  }
+
+  try {
+    const getViewerResponse = await MillicastDirector.getSubscriber(streamId, streamAccountId)
+    const options = {
+      subscriberData: getViewerResponse,
+      disableVideo: disableVideo,
+      disableAudio: disableAudio,
+    };
+    millicastView = newViewer()
+    await millicastView.connect(options);
+  } catch (error) {
+    if (!autoReconnect) return;
+
+    close().then(() => {
+      subscribe();
+    });
+    console.error(error);
+  }
+};
 
 document.addEventListener("DOMContentLoaded", () => {
-  let mainView = document.querySelector("#mainView");
-  let fullBtn = document.querySelector("#fullBtn");
-  let muteBtn = document.querySelector("#muteBtn");
-  let playBtn = document.querySelector("#playBtn");
-  let video = document.querySelector("video");
-
   let int;
   let lastclientX, lastclientY;
 
-  const toggleFullscreen = () => {
-    let fullIcon = fullBtn.children[0];
-    if (!document.fullscreenElement) {
-      document.documentElement.requestFullscreen();
-      fullIcon.classList.remove("fa-compress");
-      fullIcon.classList.add("fa-expand");
-    } else {
-      if (document.exitFullscreen) {
-        document.exitFullscreen();
-
-        fullIcon.classList.remove("fa-expand");
-        fullIcon.classList.add("fa-compress");
-      }
-    }
-  };
-  const togglePlay = () => {
-    if (video.paused) {
-      video
-        .play()
-        .then(() => {
-          togglePlayUI(true);
-        })
-        .catch(() => {});
-    } else {
-      video.pause();
-      togglePlayUI(false);
-    }
-  };
-  const togglePlayUI = (boolean) => {
-    /*let playIcon = document.querySelector('#playBtn').children[0];
-    //console.log('togglePlay',boolean ,  playIcon.classList.contains('fa-pause'));
-
-    if (boolean === true) {
-      playIcon.classList.remove('fa-play');
-      playIcon.classList.add('fa-pause');
-    } else {
-      playIcon.classList.remove('fa-pause');
-      playIcon.classList.add('fa-play');
-    }*/
-  };
-  const toggleMute = () => {
-    /*let muteIcon = muteBtn.children[0];
-    if(video.muted){
-      muteIcon.classList.remove('fa-volume-mute');
-      muteIcon.classList.add('fa-volume-down');
-    }else {
-      muteIcon.classList.remove('fa-volume-down');
-      muteIcon.classList.add('fa-volume-mute');
-    }
-
-    video.muted = !video.muted;*/
-  };
-  const displayUI = () => {
-    /*mainView.classList.remove('nocursor');
-    fullBtn.classList.remove('hidden');
-    muteBtn.classList.remove('hidden');
-    playBtn.classList.remove('hidden');
-
-    fullBtn.classList.add('visible');
-    muteBtn.classList.add('visible');
-    playBtn.classList.add('visible');*/
-  };
-  const hideUI = () => {
-    /*mainView.classList.add('nocursor');
-    fullBtn.classList.add('hidden');
-    muteBtn.classList.add('hidden');
-    playBtn.classList.add('hidden');
-
-    fullBtn.classList.remove('visible');
-    muteBtn.classList.remove('visible');
-    playBtn.classList.remove('visible');*/
-  };
-  const enableUI = () => {
-    /*fullBtn.classList.remove('not-allowed');
-    muteBtn.classList.remove('not-allowed');
-    playBtn.classList.remove('not-allowed');*/
-  };
-  let hasListeners = false;
-  const addListeners = () => {
-    /*muteBtn.addEventListener('click', toggleMute);
-    playBtn.addEventListener('click', togglePlay);
-    hasListeners = true;*/
-  };
-  const removeListeners = () => {
-    /*if (muteBtn.removeEventListener) {                   // For all major browsers, except IE 8 and earlier
-      muteBtn.removeEventListener('click', toggleMute);
-    } else if (x.detachEvent) {                    // For IE 8 and earlier versions
-      muteBtn.detachEvent('click', toggleMute);
-    }
-
-    if (playBtn.removeEventListener) {                   // For all major browsers, except IE 8 and earlier
-      playBtn.removeEventListener('click', toggleMute);
-    } else if (x.detachEvent) {                    // For IE 8 and earlier versions
-      playBtn.detachEvent('click', toggleMute);
-    }*/
-  };
   const startInt = (evt) => {
     if (int) clearInterval(int);
     int = setInterval(() => {
       let clientX = evt.clientX;
       let clientY = evt.clientY;
       if (clientX === lastclientX && clientY === lastclientY) {
-        hideUI();
         clearInterval(int);
       } else {
         lastclientX = clientX;
@@ -169,125 +176,53 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     }, 1000);
   };
-  const addStream = (stream) => {
-    //Create new video element
-    playing = true;
-    const audio = document.querySelector("audio");
-
-    if (disableVideo) {
-      if (audio) audio.srcObject = stream;
-      if (video) video.parentNode.removeChild(video);
-      togglePlay();
-    } else {
-      //Set same id
-      video.id = stream.id;
-      //Set src stream
-      //console.log('addStream');
-      if (!muted) {
-        toggleMute();
-        video.removeAttribute("muted");
-      }
-      if (!autoplay) {
-        video.autoplay = false;
-        playing = false;
-        togglePlayUI(false);
-        video.removeAttribute("autoplay");
-      }
-
-      video.srcObject = stream;
-      enableUI();
-      togglePlayUI(playing);
-      if (!hasListeners) addListeners();
-
-      if (audio) audio.parentNode.removeChild(audio);
-    }
-  };
-  const initUI = () => {
-    /*if(disableVolume){
-      muteBtn.classList.add('d-none');
-    }
-
-    if(disablePlay){
-      playBtn.classList.add('d-none');
-    }
-
-    if(disableFull){
-      fullBtn.classList.add('d-none');
-    }
-
-    displayUI();*/
-  };
-
-  const subscribe = async () => {
-    const millicastView = new MillicastView(streamId);
-    millicastView.on("broadcastEvent", (event) => {
-      if (!autoReconnect) return;
-
-      let layers = event.data["layers"] !== null ? event.data["layers"] : {};
-      if (event.name === "layers" && Object.keys(layers).length <= 0) {
-        //call play logic or being reconnect interval
-        close().then(() => {
-          subscribe();
-        });
-        console.error("Feed no longer found.");
-      }
-    });
-
-    millicastView.on("newTrack", (event) => {
-      if (!playing) addStream(event.streams[0]);
-    });
-
-    const close = () => {
-      togglePlayUI(false);
-      video.srcObject = null;
-      playing = false;
-      millicastView.millicastSignaling?.close();
-      removeListeners();
-      return Promise.resolve({});
-    };
-
-    try {
-      const getViewerResponse = await MillicastDirector.getSubscriber(streamId, streamAccountId)
-      const options = {
-        subscriberData: getViewerResponse,
-        disableVideo: disableVideo,
-        disableAudio: disableAudio,
-      };
-      await millicastView.connect(options);
-    } catch (error) {
-      if (!autoReconnect) return;
-
-      close().then(() => {
-        subscribe();
-      });
-      console.error(error);
-    }
-  };
 
   if (fullBtn) fullBtn.onclick = toggleFullscreen;
 
   video.onmousemove = (evt) => {
     startInt(evt);
-    displayUI();
   };
   video.addEventListener(
     "touchstart",
     (evt) => {
       startInt(evt);
-      displayUI();
     },
     false
   );
 
-  window.onmouseout = (evt) => {
-    if (!evt.relatedTarget) hideUI();
-  };
-
   int = setInterval(() => {
-    hideUI();
     clearInterval(int);
   }, 2000);
-
-  initUI();
   subscribe();
 });
+
+const receiverApplicationId = 'B5B8307B'
+
+window['__onGCastApiAvailable'] = function(isAvailable) {
+  if (!isAvailable) {
+    return false
+  }
+  
+  const stateChanged = cast.framework.CastContextEventType.CAST_STATE_CHANGED
+  const castContext = cast.framework.CastContext.getInstance()
+  castContext.setOptions({
+    autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED,
+    receiverApplicationId
+  })
+
+  castContext.addEventListener(stateChanged, ({castState}) => {
+    if (castState === cast.framework.CastState.NOT_CONNECTED) {
+      subscribe()
+    }
+
+    if (castState === cast.framework.CastState.CONNECTED) {
+      const castSession = castContext.getCurrentSession()
+      const mediaInfo = new chrome.cast.media.MediaInfo(streamId, '')
+      mediaInfo.customData = { streamId, streamAccountId }
+      mediaInfo.streamType = chrome.cast.media.StreamType.LIVE
+
+      const loadRequest = new chrome.cast.media.LoadRequest(mediaInfo)
+      castSession.loadMedia(loadRequest).then(close)
+    }
+  })
+}
