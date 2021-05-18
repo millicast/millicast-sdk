df36536e4e9dc082817d0fd8049abea4137c48b4
Renzo Delfino <renzo@qualabs.com>
2021-05-18 17:50:44 -0300

    Refactor scenarios to only one and reuse it for different codecs.

--- packages/millicast-sdk/tests/features/FunctionalPublish.feature
+++ packages/millicast-sdk/tests/features/FunctionalPublish.feature
@@ -1,21 +1,13 @@
 Feature: As a developer I want to publish a stream so i can ensure its working correctly
 
-  Scenario: Broadcast h264 stream
-    Given a page with broadcaster options and a page with view options
-    When I broadcast a stream with h264 codec and connect to stream as viewer
+  Scenario Outline: Broadcasting stream
+    Given a page with view options and a page with broadcaster options and codec <Codec>
+    When I broadcast a stream and connect to stream as viewer
     Then broadcast is active and Viewer receive video data
 
-  Scenario: Broadcast vp8 stream
-    Given a page with broadcaster options and a page with view options
-    When I broadcast a stream with vp8 codec and connect to stream as viewer
-    Then broadcast is active and Viewer receive video data
-
-  Scenario: Broadcast vp9 stream
-    Given a page with broadcaster options and a page with view options
-    When I broadcast a stream with vp9 codec and connect to stream as viewer
-    Then broadcast is active and Viewer receive video data
-
-  Scenario: Broadcast av1 stream
-    Given a page with broadcaster options and a page with view options
-    When I broadcast a stream with av1 codec and connect to stream as viewer
-    Then broadcast is active and Viewer receive video data
+    Examples:
+    | Codec |
+    | h264  |
+    | vp8   |
+    | vp9   |
+    | av1   |
--- packages/millicast-sdk/tests/features/step-definitions/FunctionalPublish.steps.js
+++ packages/millicast-sdk/tests/features/step-definitions/FunctionalPublish.steps.js
@@ -4,6 +4,7 @@ import { loadFeature, defineFeature } from 'jest-cucumber'
 const feature = loadFeature('../FunctionalPublish.feature', { loadRelativePath: true, errors: true })
 
 jest.setTimeout(20000)
+const pageLocation = `file:${path.join(__dirname, '../../PuppeteerJest.html')}`
 const publishToken = ''
 const streamName = ''
 const accountId = ''
@@ -36,7 +37,7 @@ beforeEach(async () => {
 })
 
 defineFeature(feature, test => {
-  test('Broadcast h264 stream', ({ given, when, then }) => {
+  test('Broadcasting stream', ({ given, when, then }) => {
     let broadcastPage
     let viewerPage
     let isActive
@@ -44,18 +45,18 @@ defineFeature(feature, test => {
     let videoFrame2
     let options
 
-    given('a page with broadcaster options and a page with view options', async () => {
+    given(/^a page with view options and a page with broadcaster options and codec (.*)$/, async (codec) => {
       broadcastPage = await browser.newPage()
       viewerPage = await browser.newPage()
-      await broadcastPage.goto(`file:${path.join(__dirname, '../../PuppeteerJest.html')}`)
-      await viewerPage.goto(`file:${path.join(__dirname, '../../PuppeteerJest.html')}`)
+      await broadcastPage.goto(pageLocation)
+      await viewerPage.goto(pageLocation)
       options = {
         ...defaultOptions,
-        codec: 'h264'
+        codec
       }
     })
 
-    when('I broadcast a stream with h264 codec and connect to stream as viewer', async () => {
+    when('I broadcast a stream and connect to stream as viewer', async () => {
       await broadcastPage.evaluate(async ({ options, publishToken, streamName }) => await startPublisher(publishToken, streamName, options), { options, publishToken, streamName })
       await viewerPage.evaluate(async ({ streamName, accountId }) => await startViewer(streamName, accountId), { streamName, accountId })
       await broadcastPage.waitForTimeout(4000)
@@ -71,124 +72,7 @@ defineFeature(feature, test => {
       expect(isActive).toBeTruthy()
       expect(videoFrame1).not.toBe(0)
       expect(videoFrame2).not.toBe(0)
-      expect(Math.abs(videoFrame1 - videoFrame2)).not.toBe(0)
-    })
-  })
-
-  test('Broadcast vp8 stream', ({ given, when, then }) => {
-    let broadcastPage
-    let viewerPage
-    let isActive
-    let videoFrame1
-    let videoFrame2
-    let options
-
-    given('a page with broadcaster options and a page with view options', async () => {
-      broadcastPage = await browser.newPage()
-      viewerPage = await browser.newPage()
-      await broadcastPage.goto(`file:${path.join(__dirname, '../../PuppeteerJest.html')}`)
-      await viewerPage.goto(`file:${path.join(__dirname, '../../PuppeteerJest.html')}`)
-      options = {
-        ...defaultOptions,
-        codec: 'vp8'
-      }
-    })
-
-    when('I broadcast a stream with vp8 codec and connect to stream as viewer', async () => {
-      await broadcastPage.evaluate(async ({ options, publishToken, streamName }) => await startPublisher(publishToken, streamName, options), { options, publishToken, streamName })
-      await viewerPage.evaluate(async ({ streamName, accountId }) => await startViewer(streamName, accountId), { streamName, accountId })
-      await broadcastPage.waitForTimeout(4000)
-
-      isActive = await broadcastPage.evaluate('window.publish.isActive()')
-
-      videoFrame1 = await viewerPage.evaluate('getVideoPixelSums()')
-      await viewerPage.waitForTimeout(500)
-      videoFrame2 = await viewerPage.evaluate('getVideoPixelSums()')
-    })
-
-    then('broadcast is active and Viewer receive video data', async () => {
-      expect(isActive).toBeTruthy()
-      expect(videoFrame1).not.toBe(0)
-      expect(videoFrame2).not.toBe(0)
-      expect(Math.abs(videoFrame1 - videoFrame2)).not.toBe(0)
-    })
-  })
-
-  test('Broadcast vp9 stream', ({ given, when, then }) => {
-    let broadcastPage
-    let viewerPage
-    let isActive
-    let videoFrame1
-    let videoFrame2
-    let options
-
-    given('a page with broadcaster options and a page with view options', async () => {
-      broadcastPage = await browser.newPage()
-      viewerPage = await browser.newPage()
-      await broadcastPage.goto(`file:${path.join(__dirname, '../../PuppeteerJest.html')}`)
-      await viewerPage.goto(`file:${path.join(__dirname, '../../PuppeteerJest.html')}`)
-      options = {
-        ...defaultOptions,
-        codec: 'vp9'
-      }
-    })
-
-    when('I broadcast a stream with vp9 codec and connect to stream as viewer', async () => {
-      await broadcastPage.evaluate(async ({ options, publishToken, streamName }) => await startPublisher(publishToken, streamName, options), { options, publishToken, streamName })
-      await viewerPage.evaluate(async ({ streamName, accountId }) => await startViewer(streamName, accountId), { streamName, accountId })
-      await broadcastPage.waitForTimeout(4000)
-
-      isActive = await broadcastPage.evaluate('window.publish.isActive()')
-
-      videoFrame1 = await viewerPage.evaluate('getVideoPixelSums()')
-      await viewerPage.waitForTimeout(500)
-      videoFrame2 = await viewerPage.evaluate('getVideoPixelSums()')
-    })
-
-    then('broadcast is active and Viewer receive video data', async () => {
-      expect(isActive).toBeTruthy()
-      expect(videoFrame1).not.toBe(0)
-      expect(videoFrame2).not.toBe(0)
-      expect(Math.abs(videoFrame1 - videoFrame2)).not.toBe(0)
-    })
-  })
-
-  test('Broadcast av1 stream', ({ given, when, then }) => {
-    let broadcastPage
-    let viewerPage
-    let isActive
-    let videoFrame1
-    let videoFrame2
-    let options
-
-    given('a page with broadcaster options and a page with view options', async () => {
-      broadcastPage = await browser.newPage()
-      viewerPage = await browser.newPage()
-      await broadcastPage.goto(`file:${path.join(__dirname, '../../PuppeteerJest.html')}`)
-      await viewerPage.goto(`file:${path.join(__dirname, '../../PuppeteerJest.html')}`)
-      options = {
-        ...defaultOptions,
-        codec: 'av1'
-      }
-    })
-
-    when('I broadcast a stream with av1 codec and connect to stream as viewer', async () => {
-      await broadcastPage.evaluate(async ({ options, publishToken, streamName }) => await startPublisher(publishToken, streamName, options), { options, publishToken, streamName })
-      await viewerPage.evaluate(async ({ streamName, accountId }) => await startViewer(streamName, accountId), { streamName, accountId })
-      await broadcastPage.waitForTimeout(4000)
-
-      isActive = await broadcastPage.evaluate('window.publish.isActive()')
-
-      videoFrame1 = await viewerPage.evaluate('getVideoPixelSums()')
-      await viewerPage.waitForTimeout(500)
-      videoFrame2 = await viewerPage.evaluate('getVideoPixelSums()')
-    })
-
-    then('broadcast is active and Viewer receive video data', async () => {
-      expect(isActive).toBeTruthy()
-      expect(videoFrame1).not.toBe(0)
-      expect(videoFrame2).not.toBe(0)
-      expect(Math.abs(videoFrame1 - videoFrame2)).not.toBe(0)
+      expect(videoFrame1).not.toEqual(videoFrame2)
     })
   })
 })
