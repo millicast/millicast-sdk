5f724ba3006cfa5d9a8e3df5c239ae67381c5a30
Renzo Delfino <renzo@qualabs.com>
2021-06-16 16:39:23 -0300

    Added set defaults values in user options when connecting. Added enabled video and audio when getting local SDP.

--- packages/millicast-sdk/src/Publish.js
+++ packages/millicast-sdk/src/Publish.js
@@ -5,6 +5,16 @@ import Signaling, { VideoCodec } from './Signaling'
 import { webRTCEvents } from './PeerConnection'
 const logger = Logger.get('Publish')
 
+const connectOptions = {
+  mediaStream: null,
+  bandwidth: 0,
+  disableVideo: false,
+  disableAudio: false,
+  codec: VideoCodec.H264,
+  simulcast: false,
+  scalabilityMode: null
+}
+
 /**
  * @class Publish
  * @extends BaseWebRTC
@@ -68,20 +78,10 @@ export default class Publish extends BaseWebRTC {
    *  console.log('Connection failed, handle error', e)
    * }
    */
-  async connect (
-    options = {
-      mediaStream: null,
-      bandwidth: 0,
-      disableVideo: false,
-      disableAudio: false,
-      codec: VideoCodec.H264,
-      simulcast: false,
-      scalabilityMode: null
-    }
-  ) {
+  async connect (options = connectOptions) {
     logger.debug('Broadcast option values: ', options)
-    this.options = options
-    if (!options.mediaStream) {
+    this.options = getOptions(options)
+    if (!this.options.mediaStream) {
       logger.error('Error while broadcasting. MediaStream required')
       throw new Error('MediaStream required')
     }
@@ -108,15 +108,18 @@ export default class Publish extends BaseWebRTC {
     await this.webRTCPeer.getRTCPeer()
     reemit(this.webRTCPeer, this, [webRTCEvents.connectionStateChange])
 
-    this.webRTCPeer.RTCOfferOptions = {
-      offerToReceiveVideo: !options.disableVideo,
-      offerToReceiveAudio: !options.disableAudio
-    }
-    const localSdp = await this.webRTCPeer.getRTCLocalSDP({ mediaStream: options.mediaStream, simulcast: options.simulcast, codec: options.codec, scalabilityMode: options.scalabilityMode })
-    let remoteSdp = await this.signaling.publish(localSdp, options.codec)
+    const localSdp = await this.webRTCPeer.getRTCLocalSDP({
+      mediaStream: this.options.mediaStream,
+      simulcast: this.options.simulcast,
+      codec: this.options.codec,
+      scalabilityMode: this.options.scalabilityMode,
+      enableAudio: !this.options.disableAudio,
+      enableVideo: !this.options.disableVideo
+    })
+    let remoteSdp = await this.signaling.publish(localSdp, this.options.codec)
 
-    if (!options.disableVideo && options.bandwidth > 0) {
-      remoteSdp = this.webRTCPeer.updateBandwidthRestriction(remoteSdp, options.bandwidth)
+    if (!this.options.disableVideo && this.options.bandwidth > 0) {
+      remoteSdp = this.webRTCPeer.updateBandwidthRestriction(remoteSdp, this.options.bandwidth)
     }
 
     await this.webRTCPeer.setRTCRemoteSDP(remoteSdp)
@@ -128,5 +131,12 @@ export default class Publish extends BaseWebRTC {
   reconnect () {
     this.options.mediaStream = this.webRTCPeer?.getTracks() ?? this.options.mediaStream
     super.reconnect()
+  }
+}
+
+const getOptions = (options) => {
+  return {
+    ...connectOptions,
+    ...options
   }
 }
