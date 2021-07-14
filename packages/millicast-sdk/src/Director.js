6eb4b42a2e2349fc511b61e8f9608a09f7765fc0
Renzo Delfino <renzo@qualabs.com>
2021-07-14 15:37:29 -0300

    Change incoming parameters to object with options. Added support for legacy parameters. Change related documentation.

--- packages/millicast-sdk/src/Director.js
+++ packages/millicast-sdk/src/Director.js
@@ -19,6 +19,20 @@ let apiEndpoint = defaultApiEndpoint
  */
 
 /**
+ * @typedef {Object} DirectorPublisherOptions
+ * @property {String} token - Millicast Publishing Token.
+ * @property {String} streamName - Millicast Stream Name.
+ * @property {("WebRtc" | "Rtmp")} [streamType] - Millicast Stream Type.
+ */
+
+/**
+ * @typedef {Object} DirectorSubscriberOptions
+ * @property {String} streamName - Millicast publisher Stream Name.
+ * @property {String} streamAccountId - Millicast Account ID.
+ * @property {String} [subscriberToken] - Token to subscribe to secure streams. If you are subscribing to an unsecure stream, you can omit this param.
+ */
+
+/**
  * Simplify API calls to find the best server and region to publish and subscribe to.
  * For security reasosn all calls will return a [JWT](https://jwt.io) token forn authentication including the required
  * socket path to connect with.
@@ -49,18 +63,18 @@ export default class Director {
 
   /**
    * Get publisher connection data.
-   * @param {String} token - Millicast Publishing Token.
-   * @param {String} streamName - Millicast Stream Name.
-   * @param {("WebRtc" | "Rtmp")} [streamType] - Millicast Stream Type.
+   * @param {DirectorPublisherOptions | String} options - Millicast options. *Deprecated, use options parameter instead* Millicast Publishing Token.
+   * @param {String} [streamName] - *Deprecated, use options parameter instead* Millicast Stream Name.
+   * @param {("WebRtc" | "Rtmp")} [streamType] - *Deprecated, use options parameter instead* Millicast Stream Type.
    * @returns {Promise<MillicastDirectorResponse>} Promise object which represents the result of getting the publishing connection path.
-   * @example const response = await Director.getPublisher(token, streamName)
+   * @example const response = await Director.getPublisher(options)
    * @example
    * import { Publish, Director } from '@millicast/sdk'
    *
    * //Define getPublisher as callback for Publish
    * const streamName = "My Millicast Stream Name"
    * const token = "My Millicast publishing token"
-   * const tokenGenerator = () => Director.getPublisher(token, streamName)
+   * const tokenGenerator = () => Director.getPublisher({token, streamName})
    *
    * //Create a new instance
    * const millicastPublish = new Publish(streamName, tokenGenerator)
@@ -77,10 +91,11 @@ export default class Director {
    * await millicastPublish.connect(broadcastOptions)
    */
 
-  static async getPublisher (token, streamName, streamType = streamTypes.WEBRTC) {
-    logger.info('Getting publisher connection path for stream name: ', streamName)
-    const payload = { streamName, streamType }
-    const headers = { Authorization: `Bearer ${token}` }
+  static async getPublisher (options, streamName = null, streamType = streamTypes.WEBRTC) {
+    const optionsParsed = getPublisherOptions(options, streamName, streamType)
+    logger.info('Getting publisher connection path for stream name: ', optionsParsed.streamName)
+    const payload = { streamName: optionsParsed.streamName, streamType: optionsParsed.streamType }
+    const headers = { Authorization: `Bearer ${optionsParsed.token}` }
     const url = `${this.getEndpoint()}/api/director/publish`
     try {
       const { data } = await axios.post(url, payload, { headers })
@@ -94,20 +109,20 @@ export default class Director {
 
   /**
    * Get subscriber connection data.
-   * @param {String} streamName - Millicast publisher Stream Name.
-   * @param {String} streamAccountId - Millicast Account ID.
-   * @param {String} [subscriberToken] - Token to subscribe to secure streams. If you are subscribing to an unsecure stream, you can omit this param.
+   * @param {DirectorSubscriberOptions | String} options - Millicast options. *Deprecated, use options parameter instead* Millicast publisher Stream Name.
+   * @param {String} [streamAccountId] - *Deprecated, use options parameter instead* Millicast Account ID.
+   * @param {String} [subscriberToken] - *Deprecated, use options parameter instead* Token to subscribe to secure streams. If you are subscribing to an unsecure stream, you can omit this param.
    * @returns {Promise<MillicastDirectorResponse>} Promise object which represents the result of getting the subscribe connection data.
-   * @example const response = await Director.getSubscriber(streamName, streamAccountId)
+   * @example const response = await Director.getSubscriber(options)
    * @example
    * import { View, Director } from '@millicast/sdk'
    *
    * //Define getSubscriber as callback for Subscribe
    * const streamName = "My Millicast Stream Name"
    * const accountId = "Millicast Publisher account Id"
-   * const tokenGenerator = () => Director.getSubscriber(streamName, accountId)
+   * const tokenGenerator = () => Director.getSubscriber({streamName, accountId})
    * //... or for an secure stream
-   * const tokenGenerator = () => Director.getSubscriber(streamName, accountId, '176949b9e57de248d37edcff1689a84a047370ddc3f0dd960939ad1021e0b744')
+   * const tokenGenerator = () => Director.getSubscriber({streamName, accountId, subscriberToken: '176949b9e57de248d37edcff1689a84a047370ddc3f0dd960939ad1021e0b744'})
    *
    * //Create a new instance
    * const millicastView = new View(streamName, tokenGenerator)
@@ -125,12 +140,14 @@ export default class Director {
    * await millicastView.connect(options)
    */
 
-  static async getSubscriber (streamName, streamAccountId, subscriberToken = null) {
-    logger.info(`Getting subscriber connection data for stream name: ${streamName} and account id: ${streamAccountId}`)
-    const payload = { streamAccountId, streamName }
+  static async getSubscriber (options, streamAccountId = null, subscriberToken = null) {
+    const optionsParsed = getSubscriberOptions(options, streamAccountId, subscriberToken)
+    logger.info(`Getting subscriber connection data for stream name: ${optionsParsed.streamName} and account id: ${optionsParsed.streamAccountId}`)
+
+    const payload = { streamAccountId: optionsParsed.streamAccountId, streamName: optionsParsed.streamName }
     let headers = {}
-    if (subscriberToken) {
-      headers = { Authorization: `Bearer ${subscriberToken}` }
+    if (optionsParsed.subscriberToken) {
+      headers = { Authorization: `Bearer ${optionsParsed.subscriberToken}` }
     }
     const url = `${this.getEndpoint()}/api/director/subscribe`
     try {
@@ -142,4 +159,28 @@ export default class Director {
       throw e
     }
   }
+}
+
+const getPublisherOptions = (options, legacyStreamName, legacyStreamType) => {
+  let parsedOptions = (typeof options === 'object') ? options : {}
+  if (Object.keys(parsedOptions).length === 0) {
+    parsedOptions = {
+      token: options,
+      streamName: legacyStreamName,
+      streamType: legacyStreamType
+    }
+  }
+  return parsedOptions
+}
+
+const getSubscriberOptions = (options, legacyStreamAccountId, legacySubscriberToken) => {
+  let parsedOptions = (typeof options === 'object') ? options : {}
+  if (Object.keys(parsedOptions).length === 0) {
+    parsedOptions = {
+      streamName: options,
+      streamAccountId: legacyStreamAccountId,
+      subscriberToken: legacySubscriberToken
+    }
+  }
+  return parsedOptions
 }
