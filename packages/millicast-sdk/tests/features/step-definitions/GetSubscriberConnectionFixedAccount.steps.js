c6a1b3a25e8a965f3391dfa158450819d967fa93
Sebastian Piquerez <sebastianp@qualabs.com>
2022-03-16 11:31:54 -0300

    Removed StreamEvents module

--- packages/millicast-sdk/tests/features/step-definitions/GetSubscriberConnectionFixedAccount.steps.js
+++ packages/millicast-sdk/tests/features/step-definitions/GetSubscriberConnectionFixedAccount.steps.js
@@ -11,7 +11,6 @@ jest.mock('../../../src/config', () => {
     default: {
       MILLICAST_DIRECTOR_ENDPOINT: 'https://director.millicast.com',
       MILLICAST_TURN_SERVER_LOCATION: 'https://turn.millicast.com/webrtc/_turn',
-      MILLICAST_EVENTS_LOCATION: 'wss://streamevents.millicast.com/ws',
       MILLICAST_FIXED_ACCOUNT_ID: 'static_accountId'
     }
   }
