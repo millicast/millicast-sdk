f9c5e23bca3cadd71d10c8cbf9f78596cc64ec23
Santiago Souto <ssout@dolby.com>
2023-09-21 11:09:13 -0300

    Remove unnecessary DOM listener

--- packages/millicast-analytics-demo/src/analytics.js
+++ packages/millicast-analytics-demo/src/analytics.js
@@ -1,7 +1,5 @@
 import * as publisher from './publisher'
 import * as subscriber from './subscriber'
 
-document.addEventListener('DOMContentLoaded', async (event) => {
-  document.getElementById('subscribe').addEventListener('click', subscriber.start)
-  document.getElementById('publish').addEventListener('click', publisher.start)
-})
+document.getElementById('subscribe').addEventListener('click', subscriber.start)
+document.getElementById('publish').addEventListener('click', publisher.start)
