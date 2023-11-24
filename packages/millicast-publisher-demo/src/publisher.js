ce9a2005dc6bd1e6e495e853408b049950979743
montanaeli <emont@dolby.com>
2023-11-24 13:18:39 -0300

    Added priority as query param on publisher app

--- packages/millicast-publisher-demo/src/publisher.js
+++ packages/millicast-publisher-demo/src/publisher.js
@@ -139,7 +139,11 @@ document.addEventListener("DOMContentLoaded", async (event) => {
 
   const BroadcastMillicastStream = async () => {
     try{
-      await millicastPublishUserMedia.connect({ bandwidth, events: events })
+      const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => searchParams.get(prop),
+      });
+      let priority = parseInt(params.priority);
+      await millicastPublishUserMedia.connect({ bandwidth, events: events, priority })
       isBroadcasting = true;
       broadcastHandler();
       setUserCount();
