f64eed733c8d172512283c4bf55b44b4a0f4b67a
Santiago Souto <ssout@dolby.com>
2023-12-29 15:46:18 -0300

    Fix circular dependency & update docs

--- packages/millicast-sdk/src/PeerConnection.js
+++ packages/millicast-sdk/src/PeerConnection.js
@@ -4,7 +4,7 @@ import PeerConnectionStats, { peerConnectionStatsEvents } from './PeerConnection
 import SdpParser from './utils/SdpParser'
 import UserAgent from './utils/UserAgent'
 import Logger from './Logger'
-import { VideoCodec, AudioCodec } from './Signaling'
+import { VideoCodec, AudioCodec } from './utils/Codecs'
 import mozGetCapabilities from './utils/FirefoxCapabilities'
 
 const logger = Logger.get('PeerConnection')
