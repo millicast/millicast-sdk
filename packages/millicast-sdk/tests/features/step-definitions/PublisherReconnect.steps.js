19830f1cb3f63efe802895cd76acbb5308b0bdb8
Santiago Souto <ssout@dolby.com>
2024-01-29 16:53:22 -0300

    Fix signaling partial mocking

--- packages/millicast-sdk/tests/features/step-definitions/PublisherReconnect.steps.js
+++ packages/millicast-sdk/tests/features/step-definitions/PublisherReconnect.steps.js
@@ -1,6 +1,6 @@
 import { loadFeature, defineFeature } from 'jest-cucumber'
 import { webRTCEvents } from '../../../src/PeerConnection'
-import Signaling, { signalingEvents } from '../../../src/Signaling'
+import { signalingEvents } from '../../../src/Signaling'
 import './__mocks__/MockRTCPeerConnection'
 import './__mocks__/MockMediaStream'
 import './__mocks__/MockBrowser'
@@ -22,11 +22,22 @@ const mockTokenGenerator = jest.fn(() => {
 
 const mediaStream = new MediaStream([{ kind: 'video' }, { kind: 'audio' }])
 
+jest.mock('../../../src/Signaling', () => {
+  const originalSignaling = jest.requireActual('../../../src/Signaling')
+
+  return {
+    __esModule: true,
+    ...originalSignaling,
+    default: class MockSignaling extends originalSignaling.default {
+      async connect () { return Promise.resolve() }
+      async publish () { return Promise.resolve('SDP') }
+    }
+  }
+})
+
 beforeEach(() => {
   jest.restoreAllMocks()
   jest.clearAllTimers()
-  jest.spyOn(Signaling.prototype, 'connect').mockImplementation(jest.fn)
-  jest.spyOn(Signaling.prototype, 'publish').mockResolvedValue('SDP')
   setTimeout = jest.spyOn(window, 'setTimeout')
   jest.isolateModules(() => {
     Publish = require('../../../src/Publish').default
