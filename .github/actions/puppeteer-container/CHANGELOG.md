d2d08fa2d6d6af95ae30febf9ed303e56afa5f88
Santiago Souto <ssout@dolby.com>
2024-02-02 16:17:14 -0300

    Remove puppeteer container

--- .github/actions/puppeteer-container/CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# puppeteer-container
-
-## 4.0.0
-### Major Changes
-
-- 34704ec: Renaming to puppeteer-container
--- .github/actions/puppeteer-container/tests.pptr.js
+++
@@ -1,9 +0,0 @@
-import { test } from '@ianwalter/bff-puppeteer'
-
-test('click handler', t => {
-  let testValue = 'foo'
-  const a = document.createElement('a')
-  a.onclick = () => (testValue = 'bar')
-  a.click()
-  t.expect(testValue).toBe('bar')
-})
