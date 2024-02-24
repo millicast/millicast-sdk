aa8f88486e059f68aeb08d6b913f41fb3f2825ff
Alba Mendez <alba.mendez@dolby.com>
2024-02-24 01:05:59 +0100

    take timecode from unregistered user data

--- packages/millicast-timecode-demo/src/player.ts
+++ packages/millicast-timecode-demo/src/player.ts
@@ -78,17 +78,9 @@ export function initializeMetadataPlayer(
     ctx.globalAlpha = 1
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height)
     if (!metadataSync.metadata) return
-    const { timestamp: ts, lastTimestamp: prevTs, timingInfo: { time_scale, num_units_in_tick } } = metadataSync.metadata
-    const ats = { ...prevTs, ...ts }
-    const fractional = (ts.n_frames * num_units_in_tick * (ts.nuit_field_based_flag ? 2 : 1) + ts.time_offset) / time_scale
+    const ts = metadataSync.metadata
 
-    const textParts: [string, boolean][] = [
-      [ ats.hours_value   !== undefined ? `${ats.hours_value.toString().padStart(2, '0')}:` : '', ts.hours_value !== undefined ],
-      [ ats.minutes_value !== undefined ? `   ${ats.minutes_value.toString().padStart(2, '0')}:` : '', ts.minutes_value !== undefined ],
-      [ ats.seconds_value !== undefined ? `      ${ats.seconds_value.toString().padStart(2, '0')}` : '', ts.seconds_value !== undefined ],
-      [                                   `        :${ts.n_frames.toString().padStart(2, '0')}`, true ],
-    ]
-    const textLength = textParts.map(x => x[0].length).reduce((a, b) => Math.max(a, b))
+    const text = new Date(Number(ts)).toISOString(), textLength = text.length
 
     ctx.font = `bold ${canvas.height * .045}px monospace`
     
@@ -106,10 +98,8 @@ export function initializeMetadataPlayer(
     ctx.textAlign = 'center'
     ctx.fillStyle = 'white'
     
-    for (const [text, present] of textParts) {
-      ctx.globalAlpha = present ? 1 : 0.5
-      ctx.fillText(text.padEnd(textLength), canvas.width * .5, canvas.height * .98)
-    }
+    //ctx.globalAlpha = present ? 1 : 0.5
+    ctx.fillText(text, canvas.width * .5, canvas.height * .98)
   }
 
   metadataSync.ready.then(() => {
--- packages/millicast-timecode-demo/src/worker.ts
+++ packages/millicast-timecode-demo/src/worker.ts
@@ -4,22 +4,18 @@ import {
   startMetadataSyncService, MessageData,
 } from "nal-extractor"
 
-type TimingInfo = NonNullable<VUIParameters['timing_info']>
-
-export interface Metadata {
-  lastTimestamp: Timestamp | undefined,
-  timestamp: Timestamp,
-  timingInfo: TimingInfo,
-}
+export type Metadata = bigint
 
 startMetadataSyncService(() => {
-  const extractor = new SEIExtractor({ enablePicTiming: true })
+  const extractor = new SEIExtractor({ enableUserDataUnregistered: true })
 
   function extractTimestamp(frame: RTCEncodedVideoFrame): MessageData<Metadata> | undefined {
     const metas = extractor.processAU(frame.data)
 
     const picTimings = metas.flatMap(meta =>
-      meta.type === SEIMessageType.PIC_TIMING ? [meta.message] : [])
+      meta.type === SEIMessageType.USER_DATA_UNREGISTERED &&
+      meta.uuid === 0x00112233445566778899AABBCCDDEEFFn
+      ? [meta.data] : [])
     if (picTimings.length > 1)
       throw new Error('multiple picture timing messages in AU')
     if (picTimings.length < 1)
@@ -27,22 +23,10 @@ startMetadataSyncService(() => {
     return picTimings[0] ? [processPicTiming(picTimings[0]), []] : undefined
   }
 
-  let lastTimestamp: Timestamp | undefined
-
-  function processPicTiming(metadata: PictureTiming) {
-    if (metadata.pic_struct !== PicStruct.PROGRESSIVE)
-      throw new Error(`invalid pic_struct ${metadata.pic_struct}`)
-    if (!metadata.timestamps || !metadata.timestamps[0] || metadata.timestamps[0].ct_type !== 0)
-      throw new Error(`invalid or omitted timestamp`)
-    const timestamp = metadata.timestamps[0]
-
-    const timingInfo = extractor.psManager.currentSPS?.vui_parameters?.timing_info
-    if (!timingInfo)
-      throw new Error(`no timing info in current SPS`)
-
-    const result = { lastTimestamp, timestamp, timingInfo }
-    lastTimestamp = { ...lastTimestamp, ...timestamp }
-    return result
+  function processPicTiming(x: Uint8Array) {
+    if (x.length !== 8)
+      throw new Error(`invalid payload length ${x.length}`)
+    return new DataView(x.buffer, x.byteOffset, x.byteLength).getBigUint64(0)
   }
 
   return extractTimestamp
