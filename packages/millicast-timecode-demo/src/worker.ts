a1de457313a3a5b6f4bfb142eeaa408113d613bf
Alba Mendez <alba.mendez@dolby.com>
2024-02-26 16:15:46 +0100

    renames to account for changes

--- packages/millicast-timecode-demo/src/worker.ts
+++ packages/millicast-timecode-demo/src/worker.ts
@@ -1,6 +1,5 @@
 import {
-  PicStruct, PictureTiming, PictureTimestamp as Timestamp,
-  SEIExtractor, SEIMessageType, VUIParameters,
+  SEIExtractor, SEIMessageType,
   startMetadataSyncService, MessageData,
 } from "nal-extractor"
 
@@ -12,18 +11,18 @@ startMetadataSyncService(() => {
   function extractTimestamp(frame: RTCEncodedVideoFrame): MessageData<Metadata> | undefined {
     const metas = extractor.processAU(frame.data)
 
-    const picTimings = metas.flatMap(meta =>
+    const timings = metas.flatMap(meta =>
       meta.type === SEIMessageType.USER_DATA_UNREGISTERED &&
       meta.uuid === 0x00112233445566778899AABBCCDDEEFFn
       ? [meta.data] : [])
-    if (picTimings.length > 1)
-      throw new Error('multiple picture timing messages in AU')
-    if (picTimings.length < 1)
-      console.warn('picture timing not present in AU', frame.timestamp)
-    return picTimings[0] ? [processPicTiming(picTimings[0]), []] : undefined
+    if (timings.length > 1)
+      throw new Error('multiple timecode metadata messages in AU')
+    if (timings.length < 1)
+      console.warn('timecode metadata not present in AU', frame.timestamp)
+    return timings[0] ? [processTimingMeta(timings[0]), []] : undefined
   }
 
-  function processPicTiming(x: Uint8Array) {
+  function processTimingMeta(x: Uint8Array) {
     if (x.length !== 8)
       throw new Error(`invalid payload length ${x.length}`)
     return new DataView(x.buffer, x.byteOffset, x.byteLength).getBigUint64(0)
