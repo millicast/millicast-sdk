b87f266f1c301de8c1e57364c1a479afe971ba5c
Alba Mendez <alba.mendez@dolby.com>
2024-03-13 14:20:18 +0100

    remove unregRawData

--- packages/millicast-timecode-demo/src/worker.ts
+++ packages/millicast-timecode-demo/src/worker.ts
@@ -5,14 +5,6 @@ import {
 
 export type Metadata = string
 
-function unregRawData(uuid: bigint, data: Uint8Array): Uint8Array {
-  const rawData = new Uint8Array(16 + data.length)
-  for (let i = 0; i < 16; i++)
-    rawData[15-i] = Number((uuid >> BigInt(i*8)) & 0xFFn)
-  rawData.set(data, 16)
-  return rawData
-}
-
 startMetadataSyncService(() => {
   const extractor = new SEIExtractor({ enableUserDataUnregistered: true })
   const textDecoder = new TextDecoder()
@@ -22,7 +14,7 @@ startMetadataSyncService(() => {
 
     const timings = metas.flatMap(meta =>
       meta.type === SEIMessageType.USER_DATA_UNREGISTERED
-      ? [unregRawData(meta.uuid, meta.data)] : [])
+      ? [meta.data] : [])
     if (timings.length !== 1)
       console.warn(timings.length, 'timecode messages found in AU', frame.timestamp)
     return timings[0] ? [textDecoder.decode(timings[0]), []] : undefined
