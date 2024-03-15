0f2d16fb59bba98912a08abc2f71e5e12ebf09ea
wenfeng song <wenfeng.song@dolby.com>
2024-03-15 15:54:27 +1100

    capture the output

--- check-signature.sh
+++ check-signature.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # get the command output
-output=$(git verify-commit HEAD)
+output=$(git verify-commit HEAD 2>&1)
 
 if [ -z "$output" ]; then
     echo "\033[0;31mError: Commit is not signed.\033[0m"
