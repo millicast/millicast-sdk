112eb4ab44417d149bb4f7fbecf642c997199e1a
wenfeng song <wenfeng.song@dolby.com>
2024-03-15 14:59:50 +1100

    Revert "no signature test"
    This reverts commit b7d318be75e5d7f75150b7383329fa5dc6c253b2.

--- check-signature.sh
+++ check-signature.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # Check if the commit is signed
-# Comment
 if ! git rev-parse --verify HEAD >/dev/null 2>&1; then
     echo "Error: The last commit is not signed."
     echo "Please use 'git commit -S' or 'git commit --gpg-sign' to sign your commits."
