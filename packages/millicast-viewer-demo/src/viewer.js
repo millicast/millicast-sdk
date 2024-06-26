5b196f540d2f702ec9c45e46a26a9cc911146501
Shivank Dubey <shivank.dubey@dolby.com>
2024-06-26 16:33:38 -0700

    This wasn't needed

--- packages/millicast-viewer-demo/src/viewer.js
+++ packages/millicast-viewer-demo/src/viewer.js
@@ -22,7 +22,6 @@ const streamAccountId = !!href.searchParams.get("streamAccountId")
   : import.meta.env.MILLICAST_ACCOUNT_ID;
 
 const metadata = href.searchParams.get("metadata") === "true";
-console.log('****', metadata)
 const disableVideo = href.searchParams.get("disableVideo") === "true";
 const disableAudio = href.searchParams.get("disableAudio") === "true";
 const muted =
