a7ca071eb7f34ef27175c33bab0be432fd5966b8
Greg Kolb <greg.kolb@dolby.com>
2024-08-02 11:57:53 -0600

    fix tests for sdk

--- packages/millicast-sdk/jest.config.ts
+++ packages/millicast-sdk/jest.config.ts
@@ -2,10 +2,16 @@
 export default {
   displayName: 'millicast-sdk',
   preset: '../../jest.preset.js',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  clearMocks: true,
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '\\.[jt]sx?$': 'babel-jest',
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
   },
+  moduleNameMapper: {
+    '^uuid$': require.resolve('uuid')
+  },
+  resolver: './jest_resolver.js',
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/millicast-sdk',
-};
+  coverageDirectory: '../../coverage/packages/millicast-sdk'
+}
