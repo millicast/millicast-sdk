d17c01d715be06915480b2fdd7d8ceb096a57b81
Santiago Souto <ssout@dolby.com>
2024-08-28 20:11:24 -0300

    Comment out demo apps' jest config transformer

--- packages/millicast-chromecast-receiver/jest.config.ts
+++ packages/millicast-chromecast-receiver/jest.config.ts
@@ -3,9 +3,9 @@ export default {
   displayName: 'millicast-chromcast-receiver',
   preset: '../../jest.preset.js',
   // setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  transform: {
-    '^.+\\.[tj]s$': '@swc/jest',
-  },
+  // transform: {
+  //   '^.+\\.[tj]s$': '@swc/jest',
+  // },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/millicast-chromcast-receiver',
 };
--- packages/millicast-multiview-demo/jest.config.ts
+++ packages/millicast-multiview-demo/jest.config.ts
@@ -3,9 +3,9 @@ export default {
   displayName: 'millicast-multiviewer-demo',
   preset: '../../jest.preset.js',
   // setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  transform: {
-    '^.+\\.[tj]s$': '@swc/jest',
-  },
+  // transform: {
+  //   '^.+\\.[tj]s$': '@swc/jest',
+  // },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/millicast-multiviewer-demo',
 };
