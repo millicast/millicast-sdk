3ca2917a2cb32f7ba0b7c58c61596f5862f40744
Shivank Dubey <shivank.dubey@dolby.com>
2024-09-24 13:30:34 +1000

    Force the pipeilne to fail

--- packages/millicast-sdk/src/types/index.d.ts
+++ packages/millicast-sdk/src/types/index.d.ts
@@ -1540,7 +1540,7 @@ declare module '@millicast/sdk' {
     /**
      * Remove DRM configuration for a mediaId
      */
-    removeDRMConfiguration (mediaId: string): void;
+    removeDRMConfiguration (mediaId: string);
 
     /**
      * Check if there are any DRM protected Track
