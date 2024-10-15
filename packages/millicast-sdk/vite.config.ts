baf707de06620aa25a17870467928e6fb6b09fb2
greg kolb <greg.kolb@dolby.com>
2024-10-15 13:59:40 -0600

    build only es modules bundle

--- packages/millicast-sdk/vite.config.ts
+++ packages/millicast-sdk/vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
       fileName: 'millicast',
       // Change this to the formats you want to support.
       // Don't forget to update your package.json as well.
-      formats: ['es', 'cjs', 'umd'],
+      formats: ['es'],
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
