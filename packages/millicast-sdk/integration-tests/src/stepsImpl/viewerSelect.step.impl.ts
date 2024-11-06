10bb9b292e126428e6c13a57b3be683ed589accd
jwrob <jakub.wroblewski@dolby.com>
2024-11-06 14:31:45 +0100

    debugging commit to see available layers in CI run

--- packages/millicast-sdk/integration-tests/src/stepsImpl/viewerSelect.step.impl.ts
+++ packages/millicast-sdk/integration-tests/src/stepsImpl/viewerSelect.step.impl.ts
@@ -1,4 +1,5 @@
 import { ScenarioWorld, logger, runStep } from "cucumber-playwright-framework";
+import { getLayersFromEvent } from "../support-utils/events";
 
 export async function viewerSelectLayer(
   scenarioWorld: ScenarioWorld,
@@ -7,6 +8,8 @@ export async function viewerSelectLayer(
 ) {
   logger.debug(`viewerSelectLayer function was called`);
 
+  const layers = await getLayersFromEvent(scenarioWorld, actor)
+  console.log("AVAILABLE_LAYERS",layers)
   await runStep([
       `the ${actor} switch to the "Viewer" app`,
       `the ${actor} executes the "window.millicastView.select({encodingId:"${encodingId}"})" JavaScript function on the page`,
