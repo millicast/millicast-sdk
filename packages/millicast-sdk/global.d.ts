6c4554aba4432b40fe70f9e5dc58da2b32e8bca8
Shivank Dubey <shivank.dubey@dolby.com>
2025-11-11 13:07:58 +1100

    migrate configuration

---
+++ packages/millicast-sdk/global.d.ts
@@ -0,0 +1,16 @@
+export {}
+
+declare global {
+  interface RTCRtpSender {
+    createEncodedStreams?: () => {
+      readable: ReadableStream
+      writable: WritableStream
+    }
+  }
+  interface RTCRtpReceiver {
+    createEncodedStreams?: () => {
+      readable: ReadableStream
+      writable: WritableStream
+    }
+  }
+}
