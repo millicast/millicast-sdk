daece0027ef8fd24ec10c39297e261a37bdc2752
Shivank Dubey <shivank.dubey@dolby.com>
2025-11-18 15:58:31 +1100

    get tests working

--- packages/millicast-sdk/rollup.config.js
+++ packages/millicast-sdk/rollup.config.js
@@ -2,7 +2,6 @@ import webWorkerLoader from 'rollup-plugin-web-worker-loader'
 import json from '@rollup/plugin-json'
 
 export default [
-  // ES Module build (existing)
   {
     input: 'dist/index.js',
     output: {
@@ -33,43 +32,13 @@ export default [
       json()
     ]
   },
-  // UMD build for browser global usage (NEW)
   {
     input: 'dist/index.js',
     output: {
       file: 'dist/millicast.umd.js',
       format: 'umd',
-      name: 'millicast',  // This exposes window.millicast
-      globals: {
-        'events': 'events',
-        'eventemitter3': 'EventEmitter3',
-        're-emitter': 'reEmitter',
-        'transaction-manager': 'TransactionManager',
-        'jwt-decode': 'jwtDecode',
-        'Base64': 'Base64',
-        'js-base64': 'Base64',
-        'valibot': 'valibot',
-        'js-logger': 'Logger',
-        '@dolbyio/webrtc-stats': 'WebRTCStats',
-        'ua-parser-js': 'UAParser',
-        'semantic-sdp': 'SemanticSDP'
-      }
+      name: 'millicast'
     },
-    external: [
-      'events',
-      'eventemitter3',
-      're-emitter',
-      'transaction-manager',
-      'jwt-decode',
-      'Base64',
-      'js-base64',
-      'valibot',
-      'js-logger',
-      '@dolbyio/webrtc-stats',
-      'ua-parser-js',
-      'semantic-sdp',
-      './drm/rtc-drm-transform.js'
-    ],
     plugins: [
       webWorkerLoader({
         targetPlatform: 'browser',
